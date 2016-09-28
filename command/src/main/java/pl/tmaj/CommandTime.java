47dc6d4b073686b0b453a8073996610a36d8d317
Tomasz Maj <tmaj.pl@gmail.com>
2016-09-29 00:25:28 +0200

    refactoring #12

--- command/src/main/java/pl/tmaj/CommandTime.java
+++ command/src/main/java/pl/tmaj/CommandTime.java
@@ -6,10 +6,9 @@ import org.joda.time.DateTime;
 public class CommandTime implements Command {
 
     private static final String FORMAT = "YYYY/MM/dd HH:mm:ss";
-    private DateTime date = new DateTime();
 
     @Override
     public String execute() {
-        return date.toString(FORMAT);
+        return new DateTime().toString(FORMAT);
     }
 }
