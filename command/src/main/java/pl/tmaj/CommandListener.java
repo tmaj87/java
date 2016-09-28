bc846676e80ed8becb6d2b9e2059dff51032ae89
Tomasz Maj <tmaj.pl@gmail.com>
2016-09-28 22:50:14 +0200

    implementation #5

--- command/src/main/java/pl/tmaj/CommandListener.java
+++ command/src/main/java/pl/tmaj/CommandListener.java
@@ -8,6 +8,25 @@ public class CommandListener {
     private final Map<String, Integer> map = new HashMap<>();
 
     public String execute(Command command) {
+        recordCommand(command);
         return command.execute();
+    }
+
+    private void recordCommand(Command command) {
+        String name = getCommandName(command);
+        Integer value = map.get(name);
+        record(name, value);
+    }
+
+    private void record(String name, Integer value) {
+        if (value != null) {
+            map.put(name, value++);
+        } else {
+            map.put(name, 1);
+        }
+    }
+
+    private String getCommandName(Command command) {
+        return command.getClass().getName();
     }
 }
