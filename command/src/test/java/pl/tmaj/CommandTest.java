077c22da926825ded9d7f060f3e5fe7115ee3454
Tomasz Maj <tmaj.pl@gmail.com>
2016-09-28 22:04:15 +0200

    tdd

--- command/src/test/java/pl/tmaj/CommandTest.java
+++ command/src/test/java/pl/tmaj/CommandTest.java
@@ -9,23 +9,30 @@ public class CommandTest {
     public static final String IP = "IP";
 
     @Test
-    public void shouldGetTime() {
+    public void shouldExecuteTimeCommand() {
         String time = new CommandGetTime().execute();
 
         assertEquals("time", time);
     }
 
     @Test
-    public void shouldGetPing() {
+    public void shouldExecutePingCommand() {
         String ping = new CommandPing().execute();
 
         assertEquals("ping", ping);
     }
 
     @Test
-    public void shouldGetPingWithIp() {
+    public void shouldExecutePingWithIp() {
         String ping = new CommandPing().execute(IP);
 
         assertEquals("pinging " + IP, ping);
+    }
+
+    @Test
+    public void shouldExecuteCountCommand() {
+        String count = new CommandCount().execute();
+
+        assertEquals("count-1", count);
     }
 }
