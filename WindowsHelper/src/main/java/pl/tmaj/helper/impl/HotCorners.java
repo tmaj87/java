343c5631349e9d1d5e1374db3eefaeed3e6b3b55
Tomasz Maj <tmaj.pl@gmail.com>
2020-03-16 23:40:02 +0100

    java streaminfy with multi monitors

--- WindowsHelper/src/main/java/pl/tmaj/helper/impl/HotCorners.java
+++ WindowsHelper/src/main/java/pl/tmaj/helper/impl/HotCorners.java
@@ -3,13 +3,19 @@ package pl.tmaj.helper.impl;
 import pl.tmaj.helper.Helper;
 
 import java.awt.*;
+import java.util.Objects;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
+import static java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment;
 import static java.awt.MouseInfo.getPointerInfo;
-import static java.awt.Toolkit.getDefaultToolkit;
 import static java.awt.event.KeyEvent.VK_TAB;
 import static java.awt.event.KeyEvent.VK_WINDOWS;
 
 public class HotCorners implements Helper {
+
+    private static final int ACTIVATION_RANGE = 10;
 
     private final Robot robot;
     private boolean bottomRightCornerVisited;
@@ -20,26 +26,46 @@ public class HotCorners implements Helper {
 
     @Override
     public void check() {
-        bottomRight();
-    }
-
-    private void bottomRight() {
         if (inRightBottomCorner()) {
             if (!bottomRightCornerVisited) {
                 launchTaskView();
                 bottomRightCornerVisited = true;
             }
-        } else {
-            if (bottomRightCornerVisited) {
-                bottomRightCornerVisited = false;
-            }
+        } else if (bottomRightCornerVisited) {
+            bottomRightCornerVisited = false;
         }
     }
 
-    private boolean inRightBottomCorner() { // TODO: add multi display support
-        Dimension screen = getDefaultToolkit().getScreenSize();
-        Point position = getPointerInfo().getLocation();
-        return position.x + 1 == screen.width && position.y + 1 == screen.height;
+    private boolean inRightBottomCorner() {
+        Point point = getPointerInfo().getLocation();
+        Rectangle bounds = getBoundsForPoint(point);
+        return isInBottomRightCorner().test(point, bounds);
+    }
+
+    private static Rectangle getBoundsForPoint(Point point) {
+        return Stream.of(getLocalGraphicsEnvironment().getScreenDevices())
+                .map(isInBound(point))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(getLocalGraphicsEnvironment().getMaximumWindowBounds());
+    }
+
+    private static Function<GraphicsDevice, Rectangle> isInBound(Point point) {
+        return device -> {
+            for (GraphicsConfiguration configuration : device.getConfigurations()) {
+                final Rectangle bounds = configuration.getBounds();
+                if (bounds.contains(point)) {
+                    return bounds;
+                }
+            }
+            return null;
+        };
+    }
+
+    private BiPredicate<Point, Rectangle> isInBottomRightCorner() {
+        return (point, rectangle) ->
+                point.x - rectangle.x > rectangle.width - ACTIVATION_RANGE
+                        && point.y - rectangle.y > rectangle.height - ACTIVATION_RANGE;
     }
 
     private void launchTaskView() {
